1777e67bdf1fe4bd681ff70d392be24fffe7bee1
nataly-enne <ennenataly@gmail.com>
2020-03-30 20:51:57 -0300

    create: criando o frontend da pagina de cadastro e gerenciando as rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React,  {useState} from 'react';
 import './global.css';
-import Login from './pages/Login';
+import Routes from './routes';
 
 
 function App() {
   return (
-    <Login/>
+    <Routes/>
   );
 }
 
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './style.css';
+import {Link} from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
+
+import './style.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
@@ -17,10 +19,10 @@ export default function Login(){
                     <input placeholder="Sua ID"/>
                     <button type="submit" className="button">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
