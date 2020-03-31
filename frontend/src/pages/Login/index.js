e4f346813885901ecc91ad57532be908c6505cfb
nataly-enne <ennenataly@gmail.com>
2020-03-31 01:10:58 -0300

    add: conectando front com back | falta terminar a coneção do profile

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './style.css';
 
@@ -8,15 +10,37 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Login(){
+    const [id, setId] = useState();
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            const response = await api.post('sessions', {id});
+            
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('profile');
+        } catch(error){
+            alert('Falha no login. Tente novamente.')
+        }
+    }
+
     return(
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Be the Hero"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
                     
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value = {id}
+                        onChange = {e => setId(e.target.value)}
+                    />
                     <button type="submit" className="button">Entrar</button>
 
                     <Link className="back-link" to="/register">
